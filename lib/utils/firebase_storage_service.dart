495065d5f4279d3d59025fa2119a19f7ff26938c
Lukas Gerhardt <lukas.gerhardt@online.de>
2025-01-02 11:34:10 +0100

    Add logging for using the shared Storage service

--- lib/utils/firebase_storage_service.dart
+++ lib/utils/firebase_storage_service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 class FirebaseStorageService {
   static Future<String?> getDownloadUrl(String path) async {
     try {
+      Log.info("Pulling download URL for $path");
       return await FirebaseStorage.instance.ref(path).getDownloadURL();
     } catch (e) {
       Log.error('Error fetching download URL for $path: $e');
