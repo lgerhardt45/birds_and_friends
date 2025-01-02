0ae0b02aa9eb0aee1d7fbe28fe24b7165dc29769
Lukas Gerhardt <lukas.gerhardt@online.de>
2025-01-02 11:31:29 +0100

    Create dedicated service for pulling Storage files
    Because both the user avatar and the observation images will be pulled from Firebase Storage, I introduced a dedicated and shared service

---
+++ lib/utils/firebase_storage_service.dart
@@ -0,0 +1,13 @@
+import 'package:birds_and_friends/utils/logger.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseStorageService {
+  static Future<String?> getDownloadUrl(String path) async {
+    try {
+      return await FirebaseStorage.instance.ref(path).getDownloadURL();
+    } catch (e) {
+      Log.error('Error fetching download URL for $path: $e');
+      return null;
+    }
+  }
+}
--- lib/widgets/bird_image.dart
+++ lib/widgets/bird_image.dart
@@ -1,52 +1,24 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import '../utils/firebase_storage_service.dart';
 
 class BirdImage extends StatelessWidget {
-  final String imageUrl;
+  final String imagePath;
 
-  const BirdImage(this.imageUrl, {super.key});
-
-  Future<String> getDownloadUrl(String filePath) async {
-    FirebaseStorage storage = FirebaseStorage.instance;
-    Reference ref = storage.ref().child(filePath);
-    return await ref.getDownloadURL();
-  }
+  const BirdImage({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        // height: 300,
-        // width: 300,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
-          color: Colors.grey[300],
-        ),
-        child: FutureBuilder(
-          future: getDownloadUrl(imageUrl),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // Show a placeholder while loading
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              // Show an error placeholder
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.error, color: Colors.red),
-                    Text(snapshot.error.toString()),
-                  ],
-                ),
-              );
-            }
-            return Image.network(snapshot.data!);
-          },
-        ),
-      ),
+    return FutureBuilder<String?>(
+      future: FirebaseStorageService.getDownloadUrl(imagePath),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError || !snapshot.hasData) {
+          return Center(child: Icon(Icons.error));
+        } else {
+          return Image.network(snapshot.data!);
+        }
+      },
     );
   }
 }
